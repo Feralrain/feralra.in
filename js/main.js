5806ca1f987104906c32ae28796b5677ff23b57d
cowsauce <47434609+CowsauceDev@users.noreply.github.com>
2021-04-30 20:26:40 -0700

    license notice

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-// copyright notice
+/* Copyright © 2021 Feralrain - All Rights Reserved.
+* You may use, distribute and modify this code under the
+* terms of the MIT license.
+*
+* You should have received a copy of the MIT license with
+* this file.
+* See LICENSE for more information.
+*/
